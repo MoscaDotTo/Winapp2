795a158a1ae079227b385b331ba4b4ff83b54064
Robbie Ward <git@winapp2.com>
2019-01-16 14:05:48 -0500

    Add Minefield

---
+++ winapp2ool/components/Minefield.vb
@@ -0,0 +1,49 @@
+﻿'    Copyright (C) 2018-2019 Robbie Ward
+' 
+'    This file is a part of Winapp2ool
+' 
+'    Winapp2ool is free software: you can redistribute it and/or modify
+'    it under the terms of the GNU General Public License as published by
+'    the Free Software Foundation, either version 3 of the License, or
+'    (at your option) any later version.
+'
+'    Winap2ool is distributed in the hope that it will be useful,
+'    but WITHOUT ANY WARRANTY; without even the implied warranty of
+'    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+'    GNU General Public License for more details.
+'
+'    You should have received a copy of the GNU General Public License
+'    along with Winapp2ool.  If not, see <http://www.gnu.org/licenses/>.
+
+''' <summary>
+''' This module serves as a simple interface for testing new ideas that don't fit neatly into an existing module
+''' </summary>
+Module Minefield
+    Public Sub printMenu()
+        printMenuTop({"A testing ground for new ideas/features, watch your step!"})
+        print(1, "Game Entry Maker", "A meta script approach to game entries in winapp2.ini", closeMenu:=True)
+    End Sub
+
+    Public Sub handleUserInput(input As String)
+        Select Case input
+            Case "0"
+                exitCode = True
+            Case "1"
+                initModule("Game Entry Maker", AddressOf printGMMenu, AddressOf handleGMInput)
+        End Select
+    End Sub
+
+    Private Sub printGMMenu()
+        printMenuTop({"This tool will allow for a more meta approach to creating entries for games, particularly steam."})
+        print(1, "Run (Default)", "Attempt to generate entries", closeMenu:=True)
+    End Sub
+
+    Private Sub handleGMInput(input As String)
+        Select Case input
+            Case "0"
+                exitCode = True
+        End Select
+    End Sub
+
+
+End Module
