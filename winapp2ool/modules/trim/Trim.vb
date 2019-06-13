a856dee95e1b3f00d2c2f3a9e33e721068393d95
MoscaDotTo <git@winapp2.com>
2019-06-13 11:47:15 -0400

    always validate the trim file

--- winapp2ool/modules/trim/Trim.vb
+++ winapp2ool/modules/trim/Trim.vb
@@ -104,7 +104,7 @@ Public Module Trim
 
     ''' <summary>Initiates the trim after validating our ini files</summary>
     Private Sub initTrim()
-        If Not DownloadFileToTrim Then TrimFile1.validate()
+        TrimFile1.validate()
         If Not enforceFileHasContent(TrimFile1) Then Exit Sub
         Dim winapp2 = If(Not DownloadFileToTrim, New winapp2file(TrimFile1), New winapp2file(getRemoteIniFile(winapp2link)))
         clrConsole()
