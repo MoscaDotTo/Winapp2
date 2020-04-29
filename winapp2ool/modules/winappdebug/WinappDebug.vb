addf9a8691a85b987b4486a47a40517c7dff403d
MoscaDotTo <git@winapp2.com>
2020-04-29 16:55:23 -0400

    support all valid CCleaner LangSecRefs in WinappDebug

--- winapp2ool/modules/winappdebug/WinappDebug.vb
+++ winapp2ool/modules/winappdebug/WinappDebug.vb
@@ -91,7 +91,7 @@ Public Module WinappDebug
     ''' <summary> Detects short form registry paths </summary>
     Private Property shortReg As New Regex("HK(C(C$|R$|U$)|LM$|U$)")
     ''' <summary> Detects valid LangSecRef numbers </summary>
-    Private Property secRefNums As New Regex("30(0(5|6)|2([0-9])|3(0|1))")
+    Private Property secRefNums As New Regex("30(0([1-6])|2([1-9])|3([1-4]))")
     ''' <summary> Detects valid drive letter parameters </summary>
     Private Property driveLtrs As New Regex("[a-zA-z]:")
     ''' <summary> Detects potential %EnvironmentVariables% </summary>
