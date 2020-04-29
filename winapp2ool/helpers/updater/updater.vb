c4bee6c82027aa6fcf9b445f11fb9a4727e38285
MoscaDotTo <git@winapp2.com>
2020-04-29 15:17:55 -0400

    always pass any commandline arguments to the next instance of winapp2ool if auto updating

--- winapp2ool/helpers/updater/updater.vb
+++ winapp2ool/helpers/updater/updater.vb
@@ -164,7 +164,12 @@ Public Module updater
             fDelete(w2lName)
             ' Move the latest version to the current directory and launch it
             File.Move(tmpToolPath, $"{Environment.CurrentDirectory}\{w2lName}")
-            System.Diagnostics.Process.Start(w2lName)
+            Dim args = ""
+            ' Pass any args that were used to start this instance of winapp2ool over to the next instance 
+            If cmdargs.Count > 1 Then cmdargs.ForEach(Sub(arg) args += arg & ", ")
+            ' Remove the trailing comma 
+            If Not args.Length = 0 Then args = args.Remove(args.Length - 2)
+            Process.Start(w2lName, args)
             Environment.Exit(0)
         Catch ex As IOException
             handleIOException(ex)
