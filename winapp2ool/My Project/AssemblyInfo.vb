9429ce5abd21c4f2bdc0ee693d8b8f1672f7c9a9
Hazel Ward <MoscaDotTo@users.noreply.github.com>
2025-06-19 14:31:19 -0600

    copyright year bump

--- winapp2ool/My Project/AssemblyInfo.vb
+++ winapp2ool/My Project/AssemblyInfo.vb
@@ -13,7 +13,7 @@ Imports System.Runtime.InteropServices
 <Assembly: AssemblyDescription("A multitool for use with winapp2.ini")>
 <Assembly: AssemblyCompany("https://github.com/MoscaDotTo/Winapp2")>
 <Assembly: AssemblyProduct("winapp2ool")>
-<Assembly: AssemblyCopyright("Hazel Ward ©  2018-2024")>
+<Assembly: AssemblyCopyright("Hazel Ward ©  2018-2025")>
 <Assembly: AssemblyTrademark("")>
 
 <Assembly: ComVisible(False)>
