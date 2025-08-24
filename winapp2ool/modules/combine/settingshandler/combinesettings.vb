4ba19d3b055ee1080466bf409c255660a5fd4534
Hazel Ward <MoscaDotTo@users.noreply.github.com>
2025-08-24 01:02:31 -0600

    Introduce Combine module

---
+++ winapp2ool/modules/combine/settingshandler/combinesettings.vb
@@ -0,0 +1,56 @@
+﻿'    Copyright (C) 2018-2025 Hazel Ward
+' 
+'    This file is a part of Winapp2ool
+' 
+'    Winapp2ool is free software: you can redistribute it and/or modify
+'    it under the terms of the GNU General Public License as published by
+'    the Free Software Foundation, either version 3 of the License, or
+'    (at your option) any later version.
+'
+'    Winapp2ool is distributed in the hope that it will be useful,
+'    but WITHOUT ANY WARRANTY; without even the implied warranty of
+'    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+'    GNU General Public License for more details.
+'
+'    You should have received a copy of the GNU General Public License
+'    along with Winapp2ool.  If not, see <http://www.gnu.org/licenses/>.
+
+Option Strict On
+
+''' <summary>
+''' Holds the settings for the Combine module, which is responsible for combining multiple ini files
+''' potentially spread across numerous subdirectories into a single ini file <br />
+''' This module contains properties that define the target directory containing files to be combined 
+''' and the output file location for the combined ini file <br />
+''' It also tracks whether the module settings have been modified from their defaults. 
+''' </summary>
+''' 
+''' Docs last updated: 2025-08-23 
+Public Module combinesettings
+
+    ''' <summary>
+    ''' The target directory containing files to be combined <br />
+    ''' Default: <c> current directory </c>
+    ''' </summary>
+    ''' 
+    ''' Docs last updated: 2025-08-20 | Code last updated: 2025-08-20
+    Public Property CombineFile1 As New iniFile(Environment.CurrentDirectory, "")
+
+    ''' <summary>
+    ''' The output file location for the combined ini file <br />
+    ''' Default: <c> combined.ini </c> in the <c> current directory </c>
+    ''' </summary>
+    ''' 
+    ''' Docs last updated: 2025-08-20 | Code last updated: 2025-08-20
+    Public Property CombineFile3 As New iniFile(Environment.CurrentDirectory, "combined.ini")
+
+    ''' <summary>
+    ''' Indicates that the module settings have been modified from their defaults 
+    ''' <br />
+    ''' Default: <c> False </c>
+    ''' </summary>
+    ''' 
+    ''' Docs last updated: 2025-08-20 | Code last updated: 2025-08-20
+    Public Property CombineModuleSettingsChanged As Boolean = False
+
+End Module
---
+++ winapp2ool/modules/combine/settingshandler/combinesettingshandler.vb
@@ -0,0 +1,73 @@
+﻿'    Copyright (C) 2018-2025 Hazel Ward
+' 
+'    This file is a part of Winapp2ool
+' 
+'    Winapp2ool is free software: you can redistribute it and/or modify
+'    it under the terms of the GNU General Public License as published by
+'    the Free Software Foundation, either version 3 of the License, or
+'    (at your option) any later version.
+'
+'    Winapp2ool is distributed in the hope that it will be useful,
+'    but WITHOUT ANY WARRANTY; without even the implied warranty of
+'    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+'    GNU General Public License for more details.
+'
+'    You should have received a copy of the GNU General Public License
+'    along with Winapp2ool.  If not, see <http://www.gnu.org/licenses/>.
+
+Option Strict On
+
+''' <summary>
+''' Provides methods for managing the Combine module settings, including support methods for syncing to disk
+''' Also provides the function which restores the default state of the Combine module's properties
+''' </summary>
+''' 
+''' Docs last updated: 2025-08-20 | Code last updated: 2025-08-20
+Public Module combinesettingshandler
+
+    ''' <summary>
+    ''' Restores the default state of the Combine module's properties
+    ''' </summary>
+    ''' 
+    ''' Docs last updated: 2025-08-20 | Code last updated: 2025-08-20
+    Public Sub initDefaultCombineSettings()
+
+        CombineFile1.resetParams()
+        CombineFile1.Dir = Environment.CurrentDirectory
+        CombineFile3.resetParams()
+        CombineFile3.Name = "combined.ini"
+        CombineModuleSettingsChanged = False
+        restoreDefaultSettings(NameOf(Combine), AddressOf createCombineSettingsSection)
+
+    End Sub
+
+    ''' <summary>
+    ''' Assigns the module settings to Combine based on the current disk-writable settings representation
+    ''' </summary>
+    ''' 
+    ''' Docs last updated: 2025-08-20 | Code last updated: 2025-08-20
+    Public Sub getSerializedCombineSettings()
+
+        LoadModuleSettingsFromDict(NameOf(Combine), GetType(combinesettings))
+
+    End Sub
+
+    ''' <summary>
+    ''' Adds the current state of the module's settings into the disk-writable settings representation
+    ''' </summary>
+    ''' 
+    ''' <remarks>
+    ''' Most often, this is the default state of these settings
+    ''' </remarks>
+    ''' 
+    ''' Docs last updated: 2025-08-20 | Code last updated: 2025-08-20
+    Public Sub createCombineSettingsSection()
+
+        Dim settingsModule = GetType(combinesettings)
+        Dim moduleName = NameOf(Combine)
+
+        createModuleSettingsSection(moduleName, settingsModule)
+
+    End Sub
+
+End Module
