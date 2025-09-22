cbd70fe52c662be9d74b961dba63e19a694786d8
Hazel Ward (she/her) <MoscaDotTo@users.noreply.github.com>
2025-09-22 11:38:06 -0600

    update Downloader main menu remove Removed Entries.ini from the downloads list (deprecated) convert to new menu system

--- winapp2ool/modules/download/mainmenu/downloadmainmenu.vb
+++ winapp2ool/modules/download/mainmenu/downloadmainmenu.vb
@@ -14,85 +14,124 @@
 '
 '    You should have received a copy of the GNU General Public License
 '    along with Winapp2ool.  If not, see <http://www.gnu.org/licenses/>.
+
 Option Strict On
-''' <summary> Displays the Downloader Module main menu to the user and handles their input </summary>
-''' Docs last updated: 2020-09-14
+
+''' <summary> 
+''' Displays the Downloader Module main menu to the user and handles their input 
+''' </summary>
 Module downloadmainmenu
 
-    ''' <summary> Restores the default state of the module's parameters </summary>
-    ''' Docs last updated: 2020-09-14 | Code last updated: 2020-09-14
+    ''' <summary> 
+    ''' Restores the default state of the module's parameters 
+    ''' </summary>
     Private Sub initDefaultSettings()
+
         downloadFile.resetParams()
         DownloadModuleSettingsChanged = False
         restoreDefaultSettings(NameOf(Downloader), AddressOf createDownloadSettingsSection)
+
     End Sub
 
-    ''' <summary> Prints the download menu to the user </summary>
-    ''' Docs last updated: 2020-09-14 | Code last updated: 2021-11-16
+    ''' <summary> 
+    ''' Prints the download menu to the user 
+    ''' </summary>
     Public Sub printDownloadMainMenu()
-        printMenuTop({"Download files from the winapp2 GitHub"})
-        print(1, "Winapp2.ini", "Download the latest winapp2.ini")
-        print(1, "Non-CCleaner", "Download the latest non-ccleaner winapp2.ini")
-        print(1, "Winapp2ool", "Download the latest winapp2ool.exe")
-        print(1, "Removed Entries.ini", "Download only entries used to create the non-ccleaner winapp2.ini", leadingBlank:=True)
-        print(1, "Directory", "Change the save directory", trailingBlank:=True)
-        print(1, "Advanced", "Additional downloads for power users")
-        print(1, "ReadMe", "The winapp2ool ReadMe")
-        print(0, $"Save directory: {replDir(downloadFile.Dir)}", leadingBlank:=True, closeMenu:=Not DownloadModuleSettingsChanged)
-        print(2, NameOf(Downloader), cond:=DownloadModuleSettingsChanged, closeMenu:=True)
+
+        Dim MenuDesc = {"Download files from the winapp2 GitHub"}
+
+        Dim menu = MenuSection.CreateCompleteMenu(NameOf(Downloader), MenuDesc, ConsoleColor.DarkGreen)
+
+        menu.AddBlank _
+            .AddOption("Winapp2.ini", "Download the latest CCleaner-Flavor winapp2.ini") _
+            .AddOption("Non-CCleaner", "Download the latest winapp2.ini").AddBlank _
+            .AddOption("Winapp2ool", "Download the latest winapp2ool.exe").AddBlank _
+            .AddOption("Directory", "Change the save directory").AddBlank _
+            .AddOption("Advanced", "Additional downloads for power users").AddBlank _
+            .AddOption("ReadMe", "Download the top-level winapp2ool readme").AddBlank _
+            .AddColoredFileInfo("Save Directory: ", downloadFile.Dir, ConsoleColor.DarkYellow) _
+            .AddResetOpt(NameOf(Downloader), DownloadModuleSettingsChanged)
+
+        menu.Print()
+
     End Sub
 
-    ''' <summary> Handles user input for the Downloader main menu </summary>
-    ''' <param name="input"> The user's input </param>
-    ''' Docs last updated: 2020-09-14 | Code last updated: 2020-09-14
+    ''' <summary> 
+    ''' Handles user input for the Downloader main menu 
+    ''' </summary>
+    ''' 
+    ''' <param name="input"> 
+    ''' The user's input 
+    ''' </param>
     Public Sub handleDownloadUserInput(input As String)
+
         Select Case input
+
             Case "0"
+
                 exitModule()
+
             Case "1", "2"
+
                 downloadFile.Name = "winapp2.ini"
                 Dim link = If(input = "1", wa2Link, nonccLink)
                 download(downloadFile, link)
                 If downloadFile.Dir = Environment.CurrentDirectory Then checkedForUpdates = False
+
             Case "3"
+
                 ' Feature gate downloading the executable behind .NET 4.6+
-                If Not denyActionWithHeader(DotNetFrameworkOutOfDate, "This option requires a newer version of the .NET Framework") Then
-                    If Not denyActionWithHeader(cantDownloadExecutable And downloadFile.Dir = Environment.CurrentDirectory,
-                                                "Unable to download winapp2ool to the current directory, choose another directory before trying again") Then
-                        If downloadFile.Dir = Environment.CurrentDirectory Then
-                            autoUpdate()
-                        Else
-                            downloadFile.Name = "winapp2ool.exe"
-                            download(downloadFile, toolExeLink)
-                        End If
-                    End If
+                If denyActionWithHeader(DotNetFrameworkOutOfDate, "This option requires a newer version of the .NET Framework") Then Return
+                If denyActionWithHeader(cantDownloadExecutable And downloadFile.Dir = Environment.CurrentDirectory, "Unable to download winapp2ool to the current directory, choose another directory before trying again") Then Return
+
+                If downloadFile.Dir = Environment.CurrentDirectory Then
+
+                    autoUpdate()
+
+                Else
+
+                    downloadFile.Name = "winapp2ool.exe"
+                    download(downloadFile, toolExeLink)
+
                 End If
+
             Case "4"
-                downloadFile.Name = "Removed entries.ini"
-                download(downloadFile, removedLink)
-            Case "5"
+
                 Dim tmp = downloadFile.Dir
+
                 initModule("Directory Chooser", AddressOf downloadFile.printDirChooserMenu, AddressOf downloadFile.handleDirChooserInput)
+
                 Dim headerTxt = "Directory change aborted"
-                If Not tmp = downloadFile.Dir Then
-                    headerTxt = "Save directory changed"
-                    updateSettings(NameOf(Downloader), NameOf(downloadFile) & "_Dir", downloadFile.Dir)
+                setNextMenuHeaderText(headerTxt, printColor:=ConsoleColor.Red)
+
+                If tmp = downloadFile.Dir Then Return
+
+                headerTxt = "Save directory changed"
+                    setNextMenuHeaderText(headerTxt, printColor:=ConsoleColor.Green)
+
                     DownloadModuleSettingsChanged = True
+                    updateSettings(NameOf(Downloader), NameOf(downloadFile) & "_Dir", downloadFile.Dir)
                     updateSettings(NameOf(Downloader), NameOf(DownloadModuleSettingsChanged), DownloadModuleSettingsChanged.ToString(System.Globalization.CultureInfo.InvariantCulture))
-                    saveSettingsFile()
-                End If
-                setHeaderText(headerTxt)
-            Case "6"
+
+            Case "5"
+
                 initModule("Advanced Downloads", AddressOf printAdvMenu, AddressOf handleAdvInput)
-            Case "7"
-                ' It's actually a .md but the user doesn't need to know that  
-                downloadFile.Name = "Readme.txt"
+
+            Case "6"
+
+                downloadFile.Name = "readme.md"
                 download(downloadFile, readMeLink)
-            Case "8"
+
+            Case "7"
+
                 If DownloadModuleSettingsChanged Then initDefaultSettings()
+
             Case Else
-                setHeaderText(invInpStr, True)
+
+                setNextMenuHeaderText(invInpStr, printColor:=ConsoleColor.Red)
+
         End Select
+
     End Sub
 
 End Module
