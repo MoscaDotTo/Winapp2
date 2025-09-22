9b7b4caae8f3247e52e619ed555c52f0213332a3
Hazel Ward (she/her) <MoscaDotTo@users.noreply.github.com>
2025-09-22 11:14:04 -0600

    scrap the current set of minefiled experiments

--- winapp2ool/modules/minefield/tool/Minefield.vb
+++ winapp2ool/modules/minefield/tool/Minefield.vb
@@ -14,101 +14,51 @@
 '
 '    You should have received a copy of the GNU General Public License
 '    along with Winapp2ool.  If not, see <http://www.gnu.org/licenses/>.
+
 Option Strict On
+
 ''' <summary>
-''' This module serves as a simple interface for testing new ideas that don't fit neatly into an existing module
-''' The ideas here should be considered alpha and the code here should be considered spaghetti 
+''' Minefield is a winapp2ool module which serves as a testing ground for new ideas and features
+''' that don't fit into an existing module. It is not intended for general use, and the features 
+''' here may be incomplete, experimental, or broken.
 ''' </summary>
 Module Minefield
-    ''' <summary>Prints the main Minefield menu</summary>
+
+    ''' <summary>
+    ''' Prints the main Minefield menu
+    ''' </summary>
     Public Sub printMenu()
-        printMenuTop({"A testing ground for new ideas/features, watch your step!", "Some options may not do anything."})
-        print(1, "Java Entry Maker", "Clean up after the messy JRE installer")
-        print(1, "Babel", "Generate winapp2.ini entries for lang files")
-        print(1, "Outlook", "Generate winapp2.ini entries with custom outlook profile support")
-        print(1, "Executor", "Modify your filesystem with winapp2.ini")
-        print(1, "Browser Builder", "Generate browser configuration files for Chromium and Gecko based browsers")
-        print(1, "Search", "Generate lists Of sections", closeMenu:=True)
+
+        Dim menuDescriptionLines = {"A testing ground for new ideas/features, watch your step!",
+                                    "Items in this menu may be incomplete, experimental, or broken entirely",
+                                    "Watch your step!"}
+
+        Dim menu = MenuSection.CreateCompleteMenu("Minefield", menuDescriptionLines, ConsoleColor.DarkBlue)
+
+        menu.Print()
+
     End Sub
 
-    '''<summary>Prints the main Babel menu</summary>
-    Public Sub printBabelMenu()
-        printMenuTop({"This tool will attempt To generate winapp2.ini entries To remove language files from your system. Use EXTREME caution!"})
-        print(1, "Run (Default)", "Run the tool")
-    End Sub
-
-    '''<summary>Handles the user input for Babel</summary>
-    '''<param name="input">The String containing the user's input</param>
-    Public Sub handleBabelInput(input As String)
-        Select Case input
-            Case "0"
-                exitModule()
-            Case "1", ""
-
-        End Select
-    End Sub
-
-    ''' <summary>Handles user input for the main Minefield menu</summary>
-    ''' <param name="input">The String containing the user's input</param>
+    ''' <summary>
+    ''' Handles user input for the main Minefield menu
+    ''' </summary>
+    ''' 
+    ''' <param name="input">
+    ''' The user's input
+    ''' </param>
     Public Sub handleUserInput(input As String)
-        Select Case input
-            Case "0"
-                exitModule()
-            Case "1"
-                initModule("Java Entry Maker", AddressOf printJMMenu, AddressOf handleJMInput)
-            Case "2"
-                initModule("Babel", AddressOf printBabelMenu, AddressOf handleBabelInput)
-            Case "3"
-                initModule("Outooker", AddressOf printOutlookMenu, AddressOf handleOutlookInput)
-            Case "4"
-                'initModule("Executor", AddressOf printExecutorMenu, AddressOf handleExecutorInput)
-            Case "5"
-                initModule("Browser Builder", AddressOf printBrowserBuilderMenu, AddressOf handleBrowserBuilderInput)
-            Case "6"
-                initModule("Search", AddressOf printSearchMenu, AddressOf handleSearchInput)
-        End Select
-    End Sub
 
-
-    Public Sub printSearchMenu()
-        printMenuTop({"Outputs all sections that fit the given pattern In a file"})
-        print(1, "winapp2.ini", "Search winapp2.ini by section criteria", closeMenu:=True)
-    End Sub
-
-    Public Sub handleSearchInput(input As String)
         Select Case True
+
             Case input = "0"
+
                 exitModule()
-            Case input = "1" Or input.Length = 0
-                clrConsole()
-                Console.Write("Enter the Section Or LangSecRef you'd like from the file:")
-                Dim sect = Console.ReadLine
-                Dim iwinapp2 = New iniFile(Environment.CurrentDirectory, "winapp2.ini")
-                iwinapp2.validate()
-                Dim winapp2 = New winapp2file(iwinapp2)
-                Dim out = ""
-                For Each lst In winapp2.Winapp2entries
-                    For Each entry In lst
-                        checkSection(entry, entry.SectionKey, sect, out)
-                        checkSection(entry, entry.LangSecRef, sect, out)
-                    Next
-                Next
-                cwl(out)
-                crk()
+
+            Case Else
+
+                setNextMenuHeaderText(invInpStr, printColor:=ConsoleColor.DarkRed)
+
         End Select
-    End Sub
-
-    Private Sub checkSection(entry As winapp2entry, lst As keyList, sect As String, ByRef out As String)
-        If lst.KeyCount = 0 Then Exit Sub
-
-        If lst.Keys(0).Value = sect Then
-            Dim tmp = entry.dumpToListOfStrings
-            For Each line In tmp
-                out += line & Environment.NewLine
-            Next
-            out += Environment.NewLine
-        End If
-
 
     End Sub
 
