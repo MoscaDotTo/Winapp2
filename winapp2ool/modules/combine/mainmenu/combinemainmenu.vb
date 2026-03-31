862f15403f05724acc0360160353f5fb683d57af
Hazel Ward (she/her) <MoscaDotTo@users.noreply.github.com>
2026-03-31 15:24:45 -0600

    refactor combine move to modern winapp2ool features like dispatched menusection and iniFile2

--- winapp2ool/modules/combine/mainmenu/combinemainmenu.vb
+++ winapp2ool/modules/combine/mainmenu/combinemainmenu.vb
@@ -1,8 +1,7 @@
-﻿Option Strict On
-'    Copyright (C) 2018-2025 Hazel Ward
-' 
+'    Copyright (C) 2018-2026 Hazel Ward
+'
 '    This file is a part of Winapp2ool
-' 
+'
 '    Winapp2ool is free software: you can redistribute it and/or modify
 '    it under the terms of the GNU General Public License as published by
 '    the Free Software Foundation, either version 3 of the License, or
@@ -16,142 +15,75 @@
 '    You should have received a copy of the GNU General Public License
 '    along with Winapp2ool.  If not, see <http://www.gnu.org/licenses/>.
 
-Imports System.ComponentModel.Design
-Imports Microsoft.VisualBasic.Logging
+Option Strict On
 
 ''' <summary>
-''' Provides the user interface for the Combine module
+''' Displays the Combine module main menu and handles user input.
+''' Called by both <c> printCombineMainMenu </c> (to render) and <c> handleCombineUserInput </c>
+''' (to dispatch), so the displayed option numbers and the dispatch table are always in sync.
 ''' </summary>
-''' 
-''' Docs last updated: 2025-08-20 | Code last updated: 2025-08-20
-Module combinemainmenu
+Public Module combinemainmenu
 
     ''' <summary>
-    ''' Prints the Combine module menu to the user
+    ''' Builds the Combine main menu with all options and their dispatch handlers registered inline
     ''' </summary>
-    ''' 
-    ''' Docs last updated: 2025-08-20 | Code last updated: 2025-08-20
-    Public Sub printCombineMainMenu()
+    Private Function buildCombineMenu() As MenuSection
 
         Dim menuDesc = {"Combine all ini files from a target directory and its children into a single file"}
         Dim noLog = MostRecentCombineLog.Length = 0
 
-        Dim menu = MenuSection.CreateCompleteMenu(NameOf(Combine), menuDesc, ConsoleColor.DarkCyan)
-        menu.AddOption("Run (default)", "Combine ini files").AddBlank() _
-            .AddOption("Change the target directory", "File Chooser (target)") _
-            .AddOption("Change the save file location", "File Chooser (save)").AddBlank() _
-            .AddColoredOption("Log Viewer", "View the detailed Combine log", ConsoleColor.Yellow, Not noLog) _
+        Return MenuSection.CreateCompleteMenu(NameOf(Combine), menuDesc, ConsoleColor.DarkCyan) _
+            .AddDispatchedOption("Run (default)", "Combine ini files", Sub() initCombine(CombineFile1.Dir, CombineFile3)) _
+            .AddBlank() _
+            .AddDispatchedOption("Change the target directory", "File Chooser (target)",
+                Sub() changeFile2Params(CombineFile1, CombineModuleSettingsChanged, NameOf(Combine),
+                                        NameOf(CombineFile1), NameOf(CombineModuleSettingsChanged))) _
+            .AddDispatchedOption("Change the save file location", "File Chooser (save)",
+                Sub() changeFile2Params(CombineFile3, CombineModuleSettingsChanged, NameOf(Combine),
+                                        NameOf(CombineFile3), NameOf(CombineModuleSettingsChanged))) _
+            .AddBlank() _
+            .AddDispatchedColoredOption("Log Viewer", "View the detailed Combine log", ConsoleColor.Yellow,
+                Sub() printSlice(MostRecentCombineLog), Not noLog) _
             .AddBlank(Not noLog) _
-            .AddLine($"Current target directory: {replDir(CombineFile1.Dir)}") _
-            .AddLine($"Current save location:    {replDir(CombineFile3.Path)}", CombineModuleSettingsChanged) _
-            .AddResetOpt(NameOf(Combine), CombineModuleSettingsChanged)
-
-        menu.Print()
-
-    End Sub
-
-    ''' <summary>
-    ''' Handles the user input from the Combine menu
-    ''' </summary>
-    ''' 
-    ''' <param name="input">
-    ''' The user's input string
-    ''' </param>
-    ''' 
-    ''' Docs last updated: 2025-08-20 | Code last updated: 2025-08-20
-    Public Sub handleCombineUserInput(input As String)
-
-        Dim noLog = MostRecentCombineLog.Length = 0
-
-        Dim fileOpts = getFileOpts()
-        Dim fileNums = getMenuNumbering(fileOpts, 2)
-
-        Dim logViewerNum = CType(fileOpts.Count + 2, String)
-        Dim resetNum = CType(fileOpts.Count + 2 + If(noLog, 0, 1), String)
-
-        Select Case True
-
-            ' Exit 
-            ' Notes: Always "0"
-            Case input = "0"
-
-                exitModule()
-
-            ' Run (default)
-            ' Notes: Always "1", also triggered by no input if run conditions are otherwise satisfied 
-            Case (input = "1" Or input = "")
-
-                initCombine(CombineFile1.Dir, CombineFile3)
-
-            ' File Selectors
-            ' Target dir (dir only)
-            Case fileNums.Contains(input)
-
-                Dim i = CType(input, Integer) - 2
-
-                Dim fileName = fileOpts.Keys(i)
-                Dim fileObj = fileOpts(fileName)
-
-                changeFileParams(fileObj, CombineModuleSettingsChanged, NameOf(Combine),
-                                 fileName, NameOf(CombineModuleSettingsChanged))
-
-                ' This file is used only to hold the Target directory and never holds a name 
-                CombineFile1.Name = ""
-                updateSettings(NameOf(Combine), $"{NameOf(CombineFile1)}_Name", "")
-
-            ' Log Viewer
-            ' Notes: Only available after Combine has been run at least once during the current session
-            Case Not noLog AndAlso input = logViewerNum
-
-                printSlice(MostRecentCombineLog)
-
-            ' Reset settings
-            ' Notes: Only available after a setting has been changed, always comes last in the option list
-            Case CombineModuleSettingsChanged And input = resetNum
-
-                resetModuleSettings(NameOf(Combine), AddressOf initDefaultCombineSettings)
-
-            Case Else
-
-                setHeaderText(invInpStr, True)
-
-        End Select
-
-    End Sub
-
-    ''' <summary>
-    ''' Determines the current set of file selectors displayed on the menu and returns a Dictionary 
-    ''' of those options and their respective files <br />
-    ''' <br />
-    ''' The set of possible files includes:
-    ''' <list type="bullet">
-    '''     
-    '''     <item>
-    '''     Source files target directory 
-    '''     </item>
-    '''     
-    '''     <item>
-    '''     Save target
-    '''     </item>
-    '''     
-    ''' </list>
-    ''' 
-    ''' </summary>
-    ''' 
-    ''' <returns> 
-    ''' The set of <c> iniFile </c> properties currently displayed on the menu
-    ''' </returns>
-    ''' 
-    ''' Docs last updated: 2025-08-23 | Code last updated: 2025-08-23
-    Private Function getFileOpts() As Dictionary(Of String, iniFile)
-
-        Dim selectors As New Dictionary(Of String, iniFile)
-
-        selectors.Add(NameOf(CombineFile1), CombineFile1)
-        selectors.Add(NameOf(CombineFile3), CombineFile3)
-
-        Return selectors
+            .AddColoredFileInfo("Current target directory: ", CombineFile1.Dir, ConsoleColor.Cyan) _
+            .AddColoredFileInfo("Current save location:    ", CombineFile3.Path(), ConsoleColor.Cyan) _
+            .AddBlank(CombineModuleSettingsChanged) _
+            .AddDispatchedResetOpt(NameOf(Combine), CombineModuleSettingsChanged,
+                Sub() resetModuleSettings(NameOf(Combine), AddressOf InitDefaultCombineSettings))
 
     End Function
+
+    ''' <summary>
+    ''' Prints the Combine menu to the user
+    ''' </summary>
+    Public Sub printCombineMainMenu()
+
+        buildCombineMenu().Print()
+
+    End Sub
+
+    ''' <summary>
+    ''' Handles user input for the Combine menu
+    ''' </summary>
+    '''
+    ''' <param name="input">
+    ''' The user's input
+    ''' </param>
+    Public Sub handleCombineUserInput(input As String)
+
+        Dim intInput As Integer
+
+        If Not Integer.TryParse(input, intInput) Then
+
+            setNextMenuHeaderText(invInpStr, printColor:=ConsoleColor.Red)
+            Return
+
+        End If
+
+        If intInput = 0 Then exitModule() : Return
+
+        If Not buildCombineMenu().Dispatch(intInput) Then setNextMenuHeaderText(invInpStr, printColor:=ConsoleColor.Red)
+
+    End Sub
 
 End Module
--- winapp2ool/modules/combine/tool/Combine.vb
+++ winapp2ool/modules/combine/tool/Combine.vb
@@ -1,7 +1,7 @@
-﻿'    Copyright (C) 2018-2025 Hazel Ward
-' 
+'    Copyright (C) 2018-2026 Hazel Ward
+'
 '    This file is a part of Winapp2ool
-' 
+'
 '    Winapp2ool is free software: you can redistribute it and/or modify
 '    it under the terms of the GNU General Public License as published by
 '    the Free Software Foundation, either version 3 of the License, or
@@ -20,12 +20,12 @@ Option Strict On
 Imports System.IO
 
 ''' <summary>
-''' Combine is a winapp2ool module that takes all files with the ini extension within a target 
-''' directory (including its subdirectories) and combines them into a single ini file. When 
+''' Combine is a winapp2ool module that takes all files with the ini extension within a target
+''' directory (including its subdirectories) and combines them into a single ini file. When
 ''' duplicate section names are encountered, their unique keys are merged together into the output.
-''' <br/> 
+''' <br />
 ''' Files that cannot be parsed or have no sections are ignored.
-''' <br/> <br/>
+''' <br /><br />
 ''' If the final combined output contains no sections, it will not be saved to disk
 ''' </summary>
 Public Module Combine
@@ -37,7 +37,7 @@ Public Module Combine
 
     ''' <summary>
     ''' The phrase that marks the beginning of a Combine operation in the global log
-    ''' </summary> 
+    ''' </summary>
     Public Const CombineLogStartPhrase As String = "Combining files from"
 
     ''' <summary>
@@ -45,28 +45,44 @@ Public Module Combine
     ''' </summary>
     Public Const CombineLogEndPhrase As String = "Combination complete!"
 
-
     ''' <summary>
     ''' Handles command line arguments for the Combine module
     ''' </summary>
+    '''
+    ''' <remarks>
+    ''' File arguments:
+    ''' <list type="bullet">
+    ''' <item><c> -1d path </c> — Set the target directory</item>
+    ''' <item><c> -3d path </c> — Set the output directory</item>
+    ''' <item><c> -3f name </c> — Set the output file name</item>
+    ''' </list>
+    ''' </remarks>
     Public Sub handleCmdLine()
 
-        initDefaultCombineSettings()
+        InitDefaultCombineSettings()
 
-        Dim targetDir As New iniFile With {.Dir = CombineFile1.Dir, .Name = CombineFile1.Name}
-        Dim outputFile As New iniFile With {.Dir = CombineFile3.Dir, .Name = CombineFile3.Name}
+        Dim spec As New CliArgSpec(NameOf(Combine))
+        spec.WithFile(1, CombineFile1, "targetdir") _
+            .WithFile(3, CombineFile3, "output") _
+            .Parse()
 
-        getFileAndDirParams({targetDir, New iniFile, outputFile})
-
-        initCombine(targetDir.Dir, outputFile)
+        initCombine(CombineFile1.Dir, CombineFile3)
 
     End Sub
 
     ''' <summary>
-    ''' Initializes the combine process and validates the target directory
+    ''' Initializes the combine process, validates the target directory, and displays the results
     ''' </summary>
+    '''
+    ''' <param name="targetDir">
+    ''' The directory to scan for <c> .ini </c> files
+    ''' </param>
+    '''
+    ''' <param name="outputFile">
+    ''' The <c> iniFileChooser </c> describing where the combined output will be saved
+    ''' </param>
     Public Sub initCombine(targetDir As String,
-                           outputFile As iniFile)
+                           outputFile As iniFileChooser)
 
         clrConsole()
 
@@ -77,8 +93,8 @@ Public Module Combine
 
         End If
 
-
         Dim CombineUserOutput As New MenuSection
+        Dim combinedOutput As iniFile2 = iniFile2.Empty(outputFile.Dir, outputFile.Name)
 
         Dim outputHeader = $"{CombineLogStartPhrase} {targetDir}"
         gLog(outputHeader, ascend:=True, leadr:=True)
@@ -86,7 +102,7 @@ Public Module Combine
         CombineUserOutput.AddLine(outputHeader, centered:=True)
         CombineUserOutput.AddDivider()
 
-        processCombine(CombineUserOutput, targetDir, outputFile)
+        processCombine(CombineUserOutput, targetDir, combinedOutput)
 
         CombineUserOutput.AddAnyKeyPrompt()
 
@@ -98,24 +114,24 @@ Public Module Combine
     End Sub
 
     ''' <summary>
-    ''' Processes all files in the target directory and combines them into a single INI file
+    ''' Processes all files in the target directory and combines them into a single ini file
     ''' </summary>
-    ''' 
+    '''
     ''' <param name="outputMenu">
     ''' The <c> MenuSection </c> containing the module's output to be displayed to the user
     ''' </param>
-    ''' 
+    '''
     ''' <param name="targetDir">
     ''' The parent directory potentially containing the ini files to combine
     ''' </param>
-    ''' 
-    ''' <param name="outputFile">
-    ''' The <c> iniFile </c> into which all other ini files found in 
-    ''' <c> <paramref name="targetDir"/> </c> will be merged 
+    '''
+    ''' <param name="combinedOutput">
+    ''' The <c> iniFile2 </c> into which all other ini files found in
+    ''' <paramref name="targetDir"/> will be merged
     ''' </param>
     Private Sub processCombine(outputMenu As MenuSection,
-                               targetDir As String,
-                         ByRef outputFile As iniFile)
+                                targetDir As String,
+                          ByRef combinedOutput As iniFile2)
 
         Dim allINIFiles = Directory.GetFiles(targetDir, "*.ini", SearchOption.AllDirectories).ToList()
         allINIFiles.Sort()
@@ -132,11 +148,11 @@ Public Module Combine
             updateProgress(processedCount, allINIFiles.Count)
 
             ' Don't process the output file if it's in the target directory
-            If filePath = outputFile.Path Then gLog($"Output file found in target directory, skipping: {filePath}", indent:=True) : Continue For
+            If filePath = combinedOutput.Path() Then gLog($"Output file found in target directory, skipping: {filePath}", indent:=True) : Continue For
 
             Try
 
-                attemptCombine(filePath, outputFile, processedCount, validFileCount, outputMenu)
+                attemptCombine(filePath, combinedOutput, processedCount, validFileCount, outputMenu)
 
             Catch ex As Exception
 
@@ -150,15 +166,15 @@ Public Module Combine
 
         gLog($"Processed {processedCount} files, {validFileCount} contained combinable sections", indent:=True)
 
-        Dim outputIsEmpty = outputFile.Sections.Count = 0
+        Dim outputIsEmpty = combinedOutput.Count = 0
 
-        Dim emptyOutputMsg = $"No valid sections found to combine - {outputFile.Name} will not be saved"
+        Dim emptyOutputMsg = $"No valid sections found to combine - {combinedOutput.Name} will not be saved"
         gLog(emptyOutputMsg, indent:=True, cond:=outputIsEmpty)
         outputMenu.AddWarning(emptyOutputMsg, condition:=outputIsEmpty)
 
-        outputFile.overwriteToFile(outputFile.toString, Not outputIsEmpty)
+        combinedOutput.OverwriteToFile(combinedOutput.ToString(), Not outputIsEmpty)
 
-        Dim combinedCountMsg = $"Combined {validFileCount} files into {outputFile.Name} with {outputFile.Sections.Count} sections"
+        Dim combinedCountMsg = $"Combined {validFileCount} files into {combinedOutput.Name} with {combinedOutput.Count} sections"
         gLog(combinedCountMsg, indent:=True, cond:=Not outputIsEmpty)
         outputMenu.AddBlank()
         outputMenu.AddColoredLine(combinedCountMsg, ConsoleColor.Green, centered:=True, condition:=Not outputIsEmpty)
@@ -173,20 +189,22 @@ Public Module Combine
     End Sub
 
     ''' <summary>
-    ''' Handles logging of exceptions thrown during the Combine process
+    ''' Handles logging of exceptions thrown during the Combine process. <br />
+    ''' Uses a broad <c> Exception </c> catch intentionally — each file is processed independently
+    ''' and a parse failure on one file should not abort the remaining files.
     ''' </summary>
-    ''' 
+    '''
     ''' <param name="filepath">
-    ''' The path of the file during whose processing <c> <paramref name="ex"/> </c> was thrown
+    ''' The path of the file during whose processing <paramref name="ex"/> was thrown
     ''' </param>
-    ''' 
+    '''
     ''' <param name="outputMenu">
     ''' The <c> MenuSection </c> containing the Combine module's output as it will be displayed
     ''' to the user
     ''' </param>
-    ''' 
-    ''' <param name="ex">The exception thrown while Combine was processing 
-    ''' <c> <paramref name="filepath"/> </c>
+    '''
+    ''' <param name="ex">
+    ''' The exception thrown while Combine was processing <paramref name="filepath"/>
     ''' </param>
     Private Sub handleCombineException(filepath As String,
                                  ByRef outputMenu As MenuSection,
@@ -196,46 +214,45 @@ Public Module Combine
 
         gLog($"{errMsg}: {ex.Message}", indent:=True)
         outputMenu.AddWarning(errMsg)
-        outputMenu.AddWarning($"Check the winapp2ool log for more information: {GlobalLogFile.Path}")
+        outputMenu.AddWarning($"Check the winapp2ool log for more information: {GlobalLogFile.Path()}")
 
         saveGlobalLog()
 
     End Sub
 
     ''' <summary>
-    ''' Tries to combine a single ini file into the output file, logging the success of this operation
+    ''' Tries to combine a single ini file into the combined output, logging the success of this operation
     ''' </summary>
-    ''' 
+    '''
     ''' <param name="filepath">
-    ''' The path of a particular ini file to be combined 
+    ''' The path of a particular ini file to be combined
     ''' </param>
-    ''' 
-    ''' <param name="outputFile">
-    ''' The output file into which <c> <paramref name="filepath"/> </c> will be combined
+    '''
+    ''' <param name="combinedOutput">
+    ''' The output file into which <paramref name="filepath"/> will be combined
     ''' </param>
-    ''' 
+    '''
     ''' <param name="processedCount">
     ''' The number of files that have been processed so far
     ''' </param>
-    ''' 
+    '''
     ''' <param name="validFileCount">
     ''' The number of files that have been successfully combined so far
     ''' </param>
-    ''' 
+    '''
     ''' <param name="outputMenu">
     ''' The <c> MenuSection </c> containing the Combine module's output as it will be displayed
     ''' to the user
     ''' </param>
     Private Sub attemptCombine(filepath As String,
-                         ByRef outputFile As iniFile,
+                         ByRef combinedOutput As iniFile2,
                          ByRef processedCount As Integer,
                          ByRef validFileCount As Integer,
                          ByRef outputMenu As MenuSection)
 
-        Dim currentFile As New iniFile(Path.GetDirectoryName(filepath), Path.GetFileName(filepath))
-        currentFile.init()
+        Dim currentFile As iniFile2 = iniFile2.FromFile(filepath)
 
-        If currentFile.Sections.Count = 0 Then
+        If currentFile.Count = 0 Then
 
             gLog($"Skipping file with no sections: {Path.GetFileName(filepath)}", indent:=True)
             processedCount += 1
@@ -243,14 +260,14 @@ Public Module Combine
 
         End If
 
-        Dim processingMsg = $"Processing: {Path.GetFileName(filepath)} ({currentFile.Sections.Count} sections)"
+        Dim processingMsg = $"Processing: {Path.GetFileName(filepath)} ({currentFile.Count} sections)"
         gLog(processingMsg, indent:=True)
 
-        mergeFileIntoOutput(currentFile, outputFile)
+        mergeFileIntoOutput(currentFile, combinedOutput)
 
         validFileCount += 1
 
-        Dim processedMsg = $"Processed: {Path.GetFileName(filepath)} ({currentFile.Sections.Count} sections)"
+        Dim processedMsg = $"Processed: {Path.GetFileName(filepath)} ({currentFile.Count} sections)"
         gLog(processedMsg, indent:=True)
         outputMenu.AddLine(processedMsg)
 
@@ -259,11 +276,11 @@ Public Module Combine
     ''' <summary>
     ''' Updates the console with the current progress of the combination process while it runs
     ''' </summary>
-    ''' 
-    ''' <param name="processedCount"> 
-    ''' The number of files that have been processed so far 
+    '''
+    ''' <param name="processedCount">
+    ''' The number of files that have been processed so far
     ''' </param>
-    ''' 
+    '''
     ''' <param name="totalCount">
     ''' The total number of files to be processed
     ''' </param>
@@ -276,65 +293,62 @@ Public Module Combine
     End Sub
 
     ''' <summary>
-    ''' Merges the sections from a source file into the combined output file,
+    ''' Merges the sections from a source file into the combined output,
     ''' merging keys when sections with the same name already exist
     ''' </summary>
-    ''' 
+    '''
     ''' <param name="sourceFile">
     ''' The source file whose sections will be merged into the output
     ''' </param>
-    ''' 
-    ''' <param name="outputFile">
+    '''
+    ''' <param name="combinedOutput">
     ''' The combined output file that will receive the merged sections
     ''' </param>
-    Private Sub mergeFileIntoOutput(sourceFile As iniFile, ByRef outputFile As iniFile)
+    Private Sub mergeFileIntoOutput(sourceFile As iniFile2, ByRef combinedOutput As iniFile2)
 
-        For Each sectionName In sourceFile.Sections.Keys
+        For Each sourceSection In sourceFile
 
-            Dim sourceSection = sourceFile.Sections(sectionName)
+            If combinedOutput.Contains(sourceSection.Name) Then AddUniqueKeys(sourceSection, combinedOutput, sourceSection.Name) : Continue For
 
-            If outputFile.Sections.ContainsKey(sectionName) Then AddUniqueKeys(sourceSection, outputFile, sectionName) : Continue For
-
-            outputFile.Sections.Add(sectionName, sourceSection)
-            gLog($"Added new section: [{sectionName}] ({sourceSection.Keys.KeyCount} keys)", indent:=True, indAmt:=4)
+            combinedOutput.AddSection(sourceSection)
+            gLog($"Added new section: [{sourceSection.Name}] ({sourceSection.Keys.Count} keys)", indent:=True, indAmt:=4)
 
         Next
 
     End Sub
 
     ''' <summary>
-    ''' Merges keys from a source section into an existing section in the output file, preventing 
-    ''' any keys with duplicate names and values from being added
-    ''' <br />
+    ''' Merges keys from a source section into an existing section in the output, preventing
+    ''' any keys with duplicate names and values from being added. <br />
     ''' Note: Matching values with unlike names will still be added
     ''' </summary>
-    ''' 
+    '''
     ''' <param name="sourceSection">
-    ''' The <c> iniSection </c> whose contents will be merged into the output file
+    ''' The <c> iniSection2 </c> whose contents will be merged into the output
     ''' </param>
-    ''' 
-    ''' <param name="outputFile">
-    ''' The combined output file into which keys from <c> sourceSection </c> will be merged
+    '''
+    ''' <param name="combinedOutput">
+    ''' The combined output into which keys from <paramref name="sourceSection"/> will be merged
     ''' </param>
-    ''' 
+    '''
     ''' <param name="sectionName">
     ''' The name of the current section being processed
     ''' </param>
-    Private Sub AddUniqueKeys(sourceSection As iniSection,
-                              outputFile As iniFile,
-                              sectionName As String)
+    Private Sub AddUniqueKeys(sourceSection As iniSection2,
+                               combinedOutput As iniFile2,
+                               sectionName As String)
 
-        Dim existingSection = outputFile.Sections(sectionName)
+        Dim existingSection = combinedOutput.GetSection(sectionName)
 
-        Dim extantKeys As New HashSet(Of String)(existingSection.Keys.Keys.Select(Function(k) $"{k.Name.ToLowerInvariant()}={k.Value.ToLowerInvariant()}"))
+        Dim extantKeys As New HashSet(Of String)(existingSection.Keys.Select(Function(k) $"{k.Name.ToLowerInvariant()}={k.Value.ToLowerInvariant()}"))
         Dim addedKeyCount = 0
         Dim skippedKeyCount = 0
 
-        For Each sourceKey In sourceSection.Keys.Keys
+        For Each sourceKey In sourceSection.Keys
 
             Dim keyExists = extantKeys.Contains($"{sourceKey.Name.ToLowerInvariant()}={sourceKey.Value.ToLowerInvariant()}")
 
-            existingSection.Keys.add(sourceKey, Not keyExists)
+            If Not keyExists Then existingSection.AddKey(sourceKey)
             gLog($"Added {sourceKey.Name} to {existingSection.Name}")
 
             If keyExists Then
@@ -354,34 +368,42 @@ Public Module Combine
     End Sub
 
     ''' <summary>
-    ''' Facilitates combining files from outside the module's UI
+    ''' Facilitates combining files from outside the module's UI.
+    ''' Returns the combined <c> iniFile2 </c> after processing; the caller may inspect the
+    ''' result but does not need to save it — <c> processCombine </c> writes to disk automatically.
     ''' </summary>
-    ''' 
+    '''
     ''' <param name="targetDirectory">
     ''' The directory containing files to be combined
     ''' </param>
-    ''' 
-    ''' <param name="outputFile">
-    ''' The file location where the combined output will be saved
+    '''
+    ''' <param name="outputDir">
+    ''' The directory component of the output file path
     ''' </param>
-    ''' 
+    '''
+    ''' <param name="outputName">
+    ''' The filename component of the output file path
+    ''' </param>
+    '''
     ''' <returns>
-    ''' The resulting combined output file if successful, or 
-    ''' Empty <c> iniFile </c> if the target directory does not exist or otherwise lacks valid ini files
+    ''' The resulting combined <c> iniFile2 </c> if successful, or an empty <c> iniFile2 </c>
+    ''' if the target directory does not exist or otherwise lacks valid ini files
     ''' </returns>
     Public Function RemoteCombine(targetDirectory As String,
-                                  outputFile As iniFile) As iniFile
+                                   outputDir As String,
+                                   outputName As String) As iniFile2
 
         If Not Directory.Exists(targetDirectory) Then
 
             gLog($"Target directory not found: {targetDirectory}")
-            Return New iniFile
+            Return iniFile2.Empty(outputDir, outputName)
 
         End If
 
-        processCombine(New MenuSection, targetDirectory, outputFile)
+        Dim combinedOutput As iniFile2 = iniFile2.Empty(outputDir, outputName)
+        processCombine(New MenuSection, targetDirectory, combinedOutput)
 
-        Return outputFile
+        Return combinedOutput
 
     End Function
 
